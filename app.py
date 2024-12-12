17c26b5e7de668b984b014d928f4cb3695313094
yashika39 <yashika.yadav.cseds.2021@miet.ac.in>
2024-12-12 14:49:02 +0530

    app.py

--- app.py
+++ app.py
@@ -1,38 +1,34 @@
-# IMPORT STATEMENTS
 import streamlit as st
 import pandas as pd
-from PIL import Image
-import numpy as np
 import matplotlib.pyplot as plt
-import plotly.figure_factory as ff
-from sklearn.metrics import accuracy_score
+import seaborn as sns
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-import seaborn as sns
+from sklearn.metrics import accuracy_score
 
 # Load the dataset
 df = pd.read_csv("dataset.csv")
 
-# HEADINGS
+# Headings
 st.title('Diabetes Checkup')
 st.sidebar.header('Patient Data')
 st.subheader('Training Data Stats')
 st.write(df.describe())
 
-# X AND Y DATA
+# X and Y Data
 x = df.drop(['Outcome'], axis=1)
 y = df['Outcome']
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
 
-# FUNCTION
+# Function
 def user_report():
     pregnancies = st.sidebar.slider('Pregnancies', 0, 17, 3)
     glucose = st.sidebar.slider('Glucose', 0, 200, 120)
-    bp = st.sidebar.slider('Blood Pressure', 0, 122, 70)
-    skinthickness = st.sidebar.slider('Skin Thickness', 0, 100, 20)
+    bp = st.sidebar.slider('BloodPressure', 0, 122, 70)
+    skinthickness = st.sidebar.slider('SkinThickness', 0, 100, 20)
     insulin = st.sidebar.slider('Insulin', 0, 846, 79)
     bmi = st.sidebar.slider('BMI', 0, 67, 20)
-    dpf = st.sidebar.slider('Diabetes Pedigree Function', 0.0, 2.4, 0.47)
+    dpf = st.sidebar.slider('DiabetesPedigreeFunction', 0.0, 2.4, 0.47)
     age = st.sidebar.slider('Age', 21, 88, 33)
 
     user_report_data = {
@@ -48,7 +44,7 @@ def user_report():
     report_data = pd.DataFrame(user_report_data, index=[0])
     return report_data
 
-# PATIENT DATA
+# Patient Data
 user_data = user_report()
 st.subheader('Patient Data')
 st.write(user_data)
@@ -56,15 +52,15 @@ st.write(user_data)
 # Ensure the features match
 user_data = user_data[x.columns]
 
-# MODEL
+# Model
 rf = RandomForestClassifier()
 rf.fit(x_train, y_train)
 user_result = rf.predict(user_data)
 
-# VISUALISATIONS
+# Visualisations
 st.title('Visualised Patient Report')
 
-# COLOR FUNCTION
+# Color Function
 color = 'blue' if user_result[0] == 0 else 'red'
 
 # Age vs Pregnancies
@@ -137,10 +133,9 @@ plt.yticks(np.arange(0, 3, 0.2))
 plt.title('0 - Healthy & 1 - Unhealthy')
 st.pyplot(fig_dpf)
 
-# OUTPUT
+# Output
 st.subheader('Your Report: ')
 output = 'You are not Diabetic' if user_result[0] == 0 else 'You are Diabetic'
 st.title(output)
 st.subheader('Accuracy: ')
 st.write(f'{accuracy_score(y_test, rf.predict(x_test)) * 100:.2f}%')
-
